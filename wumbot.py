5052e5b032d97dd6c334f186deb72b058d0022e6
pagedMov <pagedmov@gmail.com>
2024-03-31 20:07:41 -0400

    added new commands and cleaned some things up

--- wumbot.py
+++ wumbot.py
@@ -6,6 +6,7 @@ import os
 from plexapi.myplex import MyPlexAccount
 import requests
 import json
+import re
 
 httpd = None
 plexpass = 'Wumboners!999'
@@ -13,6 +14,7 @@ plexserver = 'movserver'
 plexuser = 'page710'
 publicip = requests.get('https://api.ipify.org').text
 
+global plex
 account = MyPlexAccount(plexuser, plexpass)
 plex = account.resource(plexserver).connect()
 
@@ -20,54 +22,68 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-sessions = []
+sessions = {}
 
 
 async def session_factory(owner, sessionname, showkey, episode):
     class Session:
         def __init__(self, owner, sessionname, showkey, episode):
+            self.plex = plex
             self.owner = owner
             self.sessionname = sessionname
             self.showkey = showkey
             self.episode = episode
+            sessions[str(self.owner.id)] = self
 
         async def resume(self, ctx, episode):
-            await ctx.send(f'Resuming {episode.title}')
-            startstream(ctx, episode)
+            if isinstance(episode,int): # if episode has not been properly tied to an episode object
+                episode = self.showkey.episodes()[episode] # then tie it
+            await ctx.send(f'Starting stream at episode: {episode.title}')
+            await startstream(ctx, episode)
         
         async def next(self, ctx):
             self.episode += 1
-            episodes = plex.library.section('Video').get(self.showkey).episodes()
+            episodes = self.showkey.episodes()
             if self.episode >= len(episodes):
                 await ctx.send('No more episodes.')
                 return
             episode = episodes[self.episode]
             await ctx.send(f'Starting next episode {episode.title}')
-            startstream(ctx, episode)
+            await startstream(ctx, episode)
 
         async def previous(self, ctx):
             self.episode -= 1
-            episodes = plex.library.section('Video').get(self.showkey).episodes()
+            episodes = self.showkey.episodes()
             if self.episode < 0:
                 await ctx.send('No previous episodes.')
                 return
             episode = episodes[self.episode]
             await ctx.send(f'Starting previous episode {episode.title}')
-            startstream(ctx, episode)
+            await startstream(ctx, episode)
+        
+        async def goto(self, ctx, episodenum):
+            self.episode = episodenum - 1
+            episodes = self.showkey.episodes()
+            if episodenum not in range(len(episodes)):
+                await ctx.send('Episode not found.')
+                return
+            episode = episodes[self.episode]
+            await ctx.send(f'Going to episode {episodenum} - {episode.title}')
+            await startstream(ctx, episode)
+
         
         async def end(self, ctx):
-            await ctx.send('Session ended.')
             next = self.episode + 1
-            if next >= len(plex.library.section('Video').get(self.showkey).episodes()):
-                # Delete session file
+            if next >= len(self.showkey.episodes()):
                 os.remove(f'sessions/{self.sessionname}.txt')
                 await ctx.send('No more episodes left, session deleted.')
                 return
             with open(f'sessions/{self.sessionname}.txt', 'w') as file:
-                json.dump({'sessionname': self.sessionname, 'showkey': self.showkey, 'episode': next}, file)
+                json.dump({'sessionname': self.sessionname, 'showkey': self.showkey.title, 'episode': next}, file)
                 await ctx.send("Session data saved.")
-            sessions.remove(self)
-    return Session(sessionname, showkey, episode)
+            sessions[str(self.owner.id)] = None
+            await ctx.send('Session ended.')
+    return Session(owner, sessionname, showkey, episode)
 
         
     
@@ -82,9 +98,13 @@ async def on_guild_join(guild):
 
 
 @bot.command(help="Start a new session. Provide a session name under 20 characters. Sessions are deleted when all episodes have been watched. Sessions start at episode 1, but you can override this by typing a number after the session name.")
-async def createnewsession(ctx,sessionname=None,episodeoverride=None):
+async def createsession(ctx,sessionname=None,episodeoverride=None):
+    if str(ctx.guild.id) not in open('authservers.txt').read():
+        await ctx.send('Server not authenticated.')
+        return
     if not sessionname:
-        await ctx.send('Please enter a session name.')
+        await ctx.send('Please enter a session name after the command, i.e. !createsession <name>.')
+        return
     if len(sessionname) > 20:
         await ctx.send('Session name too long.')
         return
@@ -92,6 +112,9 @@ async def createnewsession(ctx,sessionname=None,episodeoverride=None):
         os.makedirs('sessions')
     if os.path.exists(f'sessions/{sessionname}.txt'):
         await ctx.send('Session name already exists.')
+        return
+    if str(ctx.author.id) in sessions and sessions[str(ctx.author.id)] is not None:
+        await ctx.send('User already running existing session. Close it with !endsession first.')
         return
     
     shows = plex.library.section('Video').all()
@@ -115,17 +138,29 @@ async def createnewsession(ctx,sessionname=None,episodeoverride=None):
             return
         else:
             await ctx.send('Invalid choice.')
-    newsession = await session_factory(ctx.author,sessionname, showchoice.key, 0) if not episodeoverride else await session_factory(sessionname, showchoice.key, int(episodeoverride))
-    sessions.append(newsession)
+    newsession = await session_factory(ctx.author,sessionname, showchoice, 0) if not episodeoverride else await session_factory(sessionname, showchoice.key, int(episodeoverride))
+    sessions[str(ctx.author.id)] = newsession
     await ctx.send(f'Starting new session {sessionname} at episode {newsession.episode + 1}')
-    newsession.resume(ctx, showchoice.episodes()[newsession.episode])
+    await newsession.resume(ctx, showchoice.episodes()[newsession.episode])
     newsession = None
+
+@bot.command(help="End a session. This will save the session data for later.")
+async def endsession(ctx):
+    if str(ctx.guild.id) not in open('authservers.txt').read():
+        await ctx.send('Server not authenticated.')
+        return
+    session = sessions[str(ctx.author.id)]
+    await ctx.send('Ending session...')
+    await session.end(ctx)
 
 @bot.command(help="Resume a session. You will be prompted to pick a session from the list of existing sessions.")
 async def resumesession(ctx):
-    if ctx.guild.id not in open('authservers.txt').read():
+    if str(ctx.guild.id) not in open('authservers.txt').read():
         await ctx.send('Server not authenticated.')
         return
+    if str(ctx.author.id) in sessions and sessions[str(ctx.author.id)] is not None:
+            await ctx.send('User already running existing session. Close it with !endsession first.')
+            return
     sessions = os.listdir('sessions')
     sessionstring = 'Pick a session (type the number):\n```'
     counter = 1
@@ -150,9 +185,84 @@ async def resumesession(ctx):
 
     with open(f'sessions/{sessionchoice}', 'r') as file:
         sessiondata = json.load(file)
-        session = await session_factory(sessiondata['sessionname'], sessiondata['showkey'], sessiondata['episode'])
+        showkey = None
+        for item in plex.library.section('Video').all():
+            if item.title == sessiondata['showkey']:
+                showkey = item
+        session = await session_factory(ctx.author, sessiondata['sessionname'], showkey, sessiondata['episode'])
+        sessions[str(ctx.author.id)] = session
         await session.resume(ctx, sessiondata['episode'])
+
+@bot.command(help="List all existing sessions.")
+async def listsessions(ctx):
+    if str(ctx.guild.id) not in open('authservers.txt').read():
+        await ctx.send('Server not authenticated.')
+        return
+    sessions = os.listdir('sessions')
+    sessionstring = '```'
+    for session in sessions:
+        sessionstring += f'{session}\n'
+    sessionstring += '```'
+    await ctx.send(sessionstring)
+
+@bot.command(help="Delete a session. Provide the session name.")
+async def deletesession(ctx):
+    if str(ctx.guild.id) not in open('authservers.txt').read():
+        await ctx.send('Server not authenticated.')
+        return
+    sessions = os.listdir('sessions')
+    sessionstring = 'Pick a session to delete (type the number):\n```'
+    counter = 1
+    for session in sessions:
+        sessionstring += f'{counter} - {session}\n'
+        counter += 1
+    sessionstring += '```'
+    await ctx.send(sessionstring)
+    await ctx.send('type exit to cancel')
+
+    undecided = True
+
+    while undecided:
+        sessionchoice = await bot.wait_for('message', check=lambda msg: msg.author == ctx.author)
+        if sessionchoice.content.isdigit() and int(sessionchoice.content) <= len(sessions):
+            sessionchoice = sessions[int(sessionchoice.content) - 1]
+            undecided = False
+        elif sessionchoice.content == 'exit':
+            return
+        else:
+            await ctx.send('Invalid choice.')
+    os.remove(f'sessions/{sessionchoice}')
+    await ctx.send('Session deleted.')
+
+@bot.command(help="Move to the next episode in the session.")
+async def nextepisode(ctx):
+    if str(ctx.author.id) in sessions.keys() and sessions[str(ctx.author.id)] is not None:
+        await sessions[str(ctx.author.id)].next(ctx)
+        return
+    else:
+        await ctx.send(f"No session found for user {ctx.author.name}.")
+
+@bot.command(help="Move to the previous episode in the session.")
+async def previousepisode(ctx):
+    if str(ctx.author.id) in sessions.keys() and sessions[str(ctx.author.id)] is not None:
+        await sessions[str(ctx.author.id)].previous(ctx)
+        return
+    else:
+        await ctx.send(f"No session found for user {ctx.author.name}.")
+
     
+@bot.command(help="Use this to move the session to a specific episode")
+async def gotoepisode(ctx, episode):
+    if not episode.isdigit():
+        await ctx.send("Input a number.")
+        return
+    episode = int(episode)
+    if str(ctx.author.id) in sessions.keys() and sessions[str(ctx.author.id)] is not None:
+        await sessions[str(ctx.author.id)].goto(ctx,episode)
+        return
+    else:
+        await ctx.send(f"No session found for user {ctx.author.name}.")
+
     
     
 @bot.command(help="Receive generic response from the bot to make sure it's listening to commands")
@@ -200,13 +310,16 @@ async def startstream(ctx,episode=None):
     
     if episode:
         await ctx.send('Attempting to start stream...')
-        url = episodechoice.getStreamURL()
-        # url = f'{url}&?audioStreamID={audiochoice}&subtitleStreamID={subtitlechoice}'
-        publicipdashes = publicip.replace('.', '-') 
-        url = url.replace('192-168-1-187', publicipdashes)
-        await ctx.send(f'Stream started at:')
-        await ctx.send(url)
-        return
+        try:
+            url = episode.getStreamURL()
+            # url = f'{url}&?audioStreamID={audiochoice}&subtitleStreamID={subtitlechoice}'
+            publicipdashes = publicip.replace('.', '-') 
+            url = re.sub(r'https://.*\.db', f'https://{publicipdashes}.db', url)
+            await ctx.send(f'Stream started at: ```{url}```')
+            return
+        except:
+            await ctx.send('Some error occured, idk')
+            return
 
     shows = plex.library.section('Video').all()
     showstring = 'Pick a show (type the number):\n```'
@@ -251,97 +364,14 @@ async def startstream(ctx,episode=None):
         else:
             await ctx.send('Invalid choice.')
 
-    # Parse the XML data
-    # xml_data = episodechoice._server.query(episodechoice.key)
-    # xml_data = ET.tostring(xml_data,encoding='utf8')
-    # xml_data = xml_data.decode('utf-8')
-    # root = ET.fromstring(xml_data)
-
-    # audiostreams = [stream.attrib for stream in root.findall('.//Stream[@streamType="2"]')]
-    # streamstring = 'Pick an audio stream (type the number):\n```'
-    # counter = 1
-    # for stream in audiostreams:
-    #     streamstring += f'{counter} - {stream.get("language")}\n'
-    #     counter += 1
-    # streamstring += '```'
-    # await ctx.send(streamstring)
-    # await ctx.send('type exit to cancel')
-
-    # undecided = True
-    
-    # while undecided:
-    #     audiochoice = await bot.wait_for('message', check=lambda msg: msg.author == ctx.author)
-    #     if audiochoice.content.isdigit():
-    #         audiochoice = audiostreams[int(audiochoice.content) - 1]['id']
-    #         undecided = False
-    #     elif audiochoice.content == 'exit':
-    #         return
-    #     else:
-    #         await ctx.send('Invalid choice.')
-    
-    # subtitlestreams = [stream.attrib for stream in root.findall('.//Stream[@streamType="3"]')]
-    # subtitlechoice = None
-    # for stream in subtitlestreams:
-    #     if 'forced' in stream and stream['forced'] == '1':
-    #         subtitlechoice = stream['id']
-    #         await ctx.send(f'Forced subtitle stream found: {stream.get("language")}')
-    #         break
-    
-    # if not subtitlechoice:
-    #     streamstring = 'Pick a subtitle stream (type the number):\n```'
-    #     counter = 1
-    #     for stream in subtitlestreams:
-    #         streamstring += f'{counter} - {stream.get("language")}\n'
-    #         counter += 1
-    #     streamstring += '```'
-    #     await ctx.send(streamstring)
-    #     await ctx.send('type exit to cancel')
-
-    #     undecided = True
-
-    #     while undecided:
-    #         subtitlechoice = await bot.wait_for('message', check=lambda msg: msg.author == ctx.author)
-    #         if subtitlechoice.content.isdigit():
-    #             subtitlechoice = subtitlestreams[int(subtitlechoice.content) - 1]['id']
-    #             undecided = False
-    #         elif subtitlechoice.content == 'exit':
-    #             return
-    #         else:
-    #             await ctx.send('Invalid choice.')
-
     
     await ctx.send('Attempting to start stream...')
     url = episodechoice.getStreamURL()
-    # url = f'{url}&?audioStreamID={audiochoice}&subtitleStreamID={subtitlechoice}'
     publicipdashes = publicip.replace('.', '-') 
     url = url.replace('192-168-1-187', publicipdashes)
-    # await ctx.send(url)
-    # with open('video_player.html', 'w') as f:
-    #     f.write(f'''
-    #     <html>
-    # <head>
-    #     <title>Video Player</title>
-    #     <link href="https://vjs.zencdn.net/7.8.4/video-js.css" rel="stylesheet" />
-    # </head>
-    # <body>
-    #     <video id='my-video' class='video-js' controls preload='auto' width='640' height='264'>
-    #         <source src="{url}" type='video/mkv'>
-    #     </video>
-    #     <script src="https://vjs.zencdn.net/7.8.4/video.js"></script>
-    # </body>
-    # </html>
-    # ''')
-
-    # urlcode = plex._token
-    # PORT = 32400
-    # if not httpd:
-    #     httpd = HTTPServer(('0.0.0.0', PORT), handler_factory(urlcode))
-    #     httpd_thread = threading.Thread(target=start_httpd, args=(httpd,))
-    #     httpd_thread.start()
-    # url = f'http://localhost:{PORT}/video_player.html?X-Plex-Token={urlcode}'
-    
-    await ctx.send(f'Stream started at:')
-    await ctx.send(url)
+    await ctx.send(f'Stream started at: ```{url}```')
+    await ctx.send('Open this link by starting VLC media player, pressing Ctrl+N and pasting the link in the text box. This will work with other media players too if they have network stream support.')
+    #await ctx.send('(Note: for some god forsaken reason, right clicking the link message and then clicking copy text does not work with VLC, the link won`t work. You have to highlight it and then do Ctrl+C. Do not ask. I have no idea.)')
 
 
 if os.getenv('DISCORD_BOT_TOKEN'):
